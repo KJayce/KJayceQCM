a03699bbba4a7c61c83e307428146729521a937e
KJayce <jaycekaje@gmail.com>
2017-06-28 13:02:09 +0200

    j

--- QCMjayce.php
+++ QCMjayce.php
@@ -1,16 +1,4 @@
 <?php
-        $option = array(
-            'name' => FILTER_SANITIZE_STRING,
-            'surname' => FILTER_SANITIZE_STRING,
-            'mail' => FILTER_VALIDATE-EMAIL,
-        );
-
-        $result = filter_input_array(INPUT_POST, $options);
-
-        if($result != null AND $result != FALSE){
-            echo 'All entries have been cleansed';
-        }else{
-            echo 'An entry is incorrect';
-        }
-
+        session_start();
+        
         
